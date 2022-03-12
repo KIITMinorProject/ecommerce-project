21b9b68c9f1c20e05161300051ff8763f0a9dd3c
Sthiti04 <sthitiswain4@gmail.com>
2022-03-12 16:40:30 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
+import React from "react";
 import './App.css';
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import Header from "./components/Header";
 function App() {
   return (
-    <div className="App">
-      <p>Hello</p>
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
