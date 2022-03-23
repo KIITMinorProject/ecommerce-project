e18e15925d74ae3d103772ecc5e53d395fa26878
Sthiti04 <sthitiswain4@gmail.com>
2022-03-23 21:05:27 +0530

    few components added

---
+++ src/components/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import Header from "./components/Header";
+import Login from "./Login";
+import { useStateValue } from "./StateProvider";
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+      <ToastContainer style={{ marginTop: "45px" }} />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,65 @@
+import React,{useState} from 'react';
+import "./Login.css";
+import {Link,useHistory} from "react-router-dom";
+
+function Login()
+{
+    const history = useHistory();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = e => {
+        e.preventDefault();
+
+        auth
+            .signInWithEmailAndPassword(email, password)
+            .then(auth => {
+                history.push('/')
+            })
+            .catch(error => alert(error.message))
+    }
+
+    const register = e => {
+        e.preventDefault();
+
+        auth
+            .createUserWithEmailAndPassword(email, password)
+            .then((auth) => {
+                // it successfully created a new user with email and password
+                if (auth) {
+                    history.push('/')
+                }
+            })
+            .catch(error => alert(error.message))
+    }
+
+    return(
+        <div className='login'>
+            <Link to='/'>
+                <img className="login__logo" src="" alt="login-logo"></img>
+            </Link>
+
+            <div className='login__container'>
+                <h1>Sign In</h1>
+
+                <form>
+                    <h5>E-mail</h5>
+                    <input type='text' value={email} onChange={e => setEmail(e.target.value)}/>
+
+                    <h5>Password</h5>
+                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
+
+                    <button type='submit' className='login_signInButton'>Sign In</button>
+                </form>
+
+                <p>
+                    By signing-in you agree to our terms and conditions.
+                </p>
+
+                <button className='login__registerButton'>Create your Account</button>
+            </div>
+        </div>
+    )
+}
+
+export default Login
